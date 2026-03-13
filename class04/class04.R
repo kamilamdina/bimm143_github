6b46660d2f28671a838e6aa918d09f644c726f77
Kamila Medina <kamedina@ucsd.edu>
2026-03-13 11:54:12 -0700

    adding classes

---
+++ class04/class04.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Class 04 "
+#' author: "Kamila Medina"
+#' date: "January 16 2026"
+#' ---
+x <- 1:50
+plot(x)
+plot(x, sin(x), typ="l", col="pink", lwd=3, xlab="kamila's graph")
+
